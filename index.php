bedb3556215802fa9b1620ef720d0e079c2e877b
Remy <relst@relst.nl>
2015-03-26 05:20:13 +0100

    add piwik

--- index.php
+++ index.php
@@ -323,6 +323,22 @@ foreach (glob("functions/*.php") as $filename) {
     }
     ?>
 
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//hosted-oswa.org/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 34]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//hosted-oswa.org/piwik/piwik.php?idsite=34" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+
 
 <script>
     
